6f12dc0f812ae7a0ca9ef3e8bf4b57b4e3e78348
Dharma <gurramdharma925@gmail.com>
2021-02-18 13:11:29 +0530

    done git codes listed here in final.py

--- final.py
+++ final.py
@@ -1,1 +1,12 @@
 print("experienced githun repo a bit successfully")
+
+#git clone
+#git add
+#git add -A or gir add .
+#git modify
+#git rm
+#git mv
+#git commit -m "message"
+#git commit , next page end to save (:wq)
+#git push
+#git pull
