a02466a49a6e3ad3b1607de6a26c522c8f7f0841
Dharma <gurramdharma925@gmail.com>
2021-02-18 13:20:15 +0530

    all set

--- final.py
+++ final.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 print("experienced github repo a bit successfully")
-=======
-print("experienced githun repo a bit successfully")
+
 
 #git clone
 #git add
 #git add -A or gir add .
-#git modify
 #git rm
 #git mv
 #git status
@@ -14,4 +11,3 @@ print("experienced githun repo a bit successfully")
 #git commit , next page end to save (:wq)
 #git push
 #git pull
->>>>>>> c4159d5be2a05316f795ec9ba53276417dc645dd
